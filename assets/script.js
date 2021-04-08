6d11c2ff14382feec5fc8d494ee3e9217c7bbfd2
zackjj <zack.j.johnston@gmail.com>
2021-04-08 12:29:12 -0700

    answers now correctly display in buttons according to question[index].answer

--- assets/script.js
+++ assets/script.js
@@ -2,52 +2,60 @@
 var secondsLeft = 60
 var timePenalty = 10
 var score = 0
+
 //index tracks how many questions have been answered
 var index = 0;
 var scores = [];
+
 //html elements - buttons
 var startButton = document.getElementById("start-button")
 startButton.addEventListener("click", startQuiz)
 var varhighScore = document.getElementById("high-scores")
 var nextButton = document.getElementById("next-button")
-nextButton.addEventListener("click", nextQuestion)
-var answerButtons = document.getElementById("answer-buttons")
+var buttonA = document.getElementById("A")
+var buttonB = document.getElementById("B")
+var buttonC = document.getElementById("C")
+var buttonD = document.getElementById("D")
 //html elements - text boxes
 var question = document.getElementById("question")
 var questionsContainer = document.getElementById("question-container")
 var intro = document.getElementById("intro")
+
 //quiz functionality
 function startQuiz() {
     console.log("started")
     startButton.classList.add('hide')
-    intro.classList.add("hide")
+    intro.classList.add('hide')
     questionsContainer.classList.remove('hide')
-    question.innerHTML = questions[index].question
+    showQuestions ()
 };
-function nextQuestion (question) {
-    question.innerText = question
+//show buttons w/answers
+function showQuestions() {
+    question.innerText = questions[index].question;
+    question.innerText = questions[index].question;
+    buttonA.innerText = questions[index].answerA;
+    buttonB.innerText = questions[index].answerB;
+    buttonC.innerText = questions[index].answerC;
+    buttonD.innerText = questions[index].answerD;
 }
-function resetState() {
 
-}
-function selectAnswer () {
-nextButton.classList.remove('hide')
-}
 // if (answers.correct == true) {
 //     score ++
 // }
 // else {
 //     timeleft - 10
 // }
+//if (index == questions.length - 1) {
+//questionContainer.classList.add('hide)
+//} 
 var questions = [
     {
         question: "What does JS stand for?",
-        answers: [
-            {text: "JavaScript", correct: true},
-            {text: "Jim's Sandwich", correct: false},
-            {text: "Jumping Spiders", correct: false},
-            {text: "Jade Sculptures", correct: false}
-        ]
+        correctAnswer: "JavaScript",
+        answerA: "JavaScript",
+        answerB: "Jim's Sandwich",
+        answerC: "Jumping Spiders",
+        answerD: "Jade Sculptures"
     },
     {
         question: "What tag do you use to to place JavaScript in?",
@@ -57,9 +65,6 @@ var questions = [
             {text: "script", correct: true},
             {text: "code", correct: false}
         ]
-    },
-    {
-        questions: ""
     }
 ]
-console.log(questions[0].answers[0].correct)
+console.log(questions[0])
