58362f80c2861fb810b6e14076ea8b40fc5175bc
zackjj <zack.j.johnston@gmail.com>
2021-04-12 12:09:00 -0700

    added html for high scores, submit score, end of quiz

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-//initial state
-var timePenalty = 10
-var score = 0
-
 //index tracks how many questions have been answered
 var index = 0;
-var scores = [];
+//
+var storedScores = JSON.parse(localStorage.getItem("scores"));
+if (!storedScores) {
+    storedScores = [];
+}
 var secondsLeft = 60
 //timer
 var timer = setInterval(startTimer, 1000) 
@@ -13,14 +13,21 @@ function startTimer () {
     secondsLeft--;
     if (secondsLeft <= 0) {
     clearInterval(timer);
-    document.getElementById("time-left").innerText = "Time's Up!";  
+    document.getElementById("time-left").innerText = "Time's Up!";
+    displayScoreScreen () 
     }
 }
+//function to display score screen, called by timer
+function showScoreScreen () {
+    questionsContainer.style.display = "none"
+    intro.style.display = "none"
 
+}
 // buttons - html and event listeners
 var startButton = document.getElementById("start-button")
 startButton.addEventListener("click", startQuiz)
-var varhighScore = document.getElementById("high-scores")
+var highScoreSubmit = document.getElementById("high-score-submit")
+highScoreSubmit.addEventListener("click", submitScore)
 var answerButtons = document.getElementsByClassName("answer-button")
 for (var i = 0; i < answerButtons.length; i++) {
   (function(x) {
@@ -35,9 +42,12 @@ var buttonD = document.getElementById("D")
 var question = document.getElementById("question")
 var questionsContainer = document.getElementById("question-container")
 var intro = document.getElementById("intro")
+var highScores = document.getElementById("high-scores")
+var quizEnd = document.getElementById("quiz-end")
 
 //quiz functionality
 function startQuiz() {
+    score = 0
     console.log("started")
     startButton.classList.add('hide')
     intro.classList.add('hide')
@@ -53,6 +63,50 @@ function showQuestions() {
     buttonB.innerText = questions[index].answerB;
     buttonC.innerText = questions[index].answerC;
     buttonD.innerText = questions[index].answerD;
+}
+//check if answer is correct, if correct add to score, go to next question
+//if incorrect, do not add to score, subtract from timer, go to next question
+function isRight () {
+    if (questions[index] <= question.length) {
+        showEnd ();
+    }
+    if (this.innerText === questions[index].correctAnswer) {
+        score++;
+        console.log("correct");
+        index++;
+        showQuestions();
+    }
+    else {
+        console.log("wrong");
+        index++;
+        secondsLeft = secondsLeft - 10;
+        document.getElementById("time-left").innerText = secondsLeft;
+        showQuestions();
+    }
+}
+//show scores
+function showHighScores () {
+    intro.display = "none"
+    questionsContainer.display = "none"
+    quizEnd.display = "none"
+    highScores.display = "block"
+    for (var i = 0; i < storedScores.length; i++) {
+        var scoreElement = document.createElement("p");
+        scoreElement.innerText = scoresFromStorage[i].initials + "recorded a score of" + scoresFromStorage[i].score;
+        scoreTable.appendChild(scoreElement)
+    }
+}
+
+function showEnd () {
+    intro.display = "none"
+    questionsContainer.display = "none"
+    quizEnd.display = "block"
+    highScores.display = "none"
+}
+
+//submit score
+function submitScore () {
+    var newUser = initials.value
 }
 var questions = [
     {
@@ -86,29 +140,5 @@ var questions = [
         answerB: "",
         answerC: "",
         answerD: "",
-    },
-    {
-        question: "",
-        correctAnswer: "",
-        answerA: "",
-        answerB: "",
-        answerC: "",
-        answerD: "",
     }
 ]
-//check if answer is correct, if correct add to score, go to next question
-//if incorrect, do not add to score, subtract from timer, go to next question
-function isRight () {
-    if (this.innerText === questions[index].correctAnswer) {
-        score++;
-        console.log("correct");
-        index++;
-        showQuestions();
-    }
-    else {
-        console.log("wrong");
-        index++;
-        secondsLeft = secondsLeft - 10;
-        showQuestions();
-    }
-}
